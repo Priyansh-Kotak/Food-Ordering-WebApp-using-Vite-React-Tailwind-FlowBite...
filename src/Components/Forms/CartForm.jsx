f9eb5e55f0a20f95138806d3cef6e323ff681a3e
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-09-23 18:12:48 +0530

    All done

--- src/Components/Forms/CartForm.jsx
+++ src/Components/Forms/CartForm.jsx
@@ -13,7 +13,7 @@ export default function Form(props) {
           id="floating_email"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
-          // required
+          required
         />
         <label
           htmlFor="floating_email"
@@ -29,7 +29,7 @@ export default function Form(props) {
           id="floating_password"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
-          // required
+          required
         />
         <label
           htmlFor="floating_password"
@@ -45,7 +45,7 @@ export default function Form(props) {
           id="floating_repeat_password"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
-          // required
+          required
         />
         <label
           htmlFor="floating_repeat_password"
@@ -62,7 +62,7 @@ export default function Form(props) {
             id="floating_first_name"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            // required
+            required
           />
           <label
             htmlFor="floating_first_name"
@@ -78,7 +78,7 @@ export default function Form(props) {
             id="floating_last_name"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            // required
+            required
           />
           <label
             htmlFor="floating_last_name"
@@ -97,7 +97,7 @@ export default function Form(props) {
             id="floating_phone"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            // required
+            required
           />
           <label
             htmlFor="floating_phone"
@@ -113,13 +113,13 @@ export default function Form(props) {
             id="floating_company"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            // required
+            required
           />
           <label
             htmlFor="floating_company"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Company (Ex. Google)
+            Current Address
           </label>
         </div>
       </div>
